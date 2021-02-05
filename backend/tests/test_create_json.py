ce0361152fe640ea6c2d4ff1915809c6be36f33b
kato.hiroaki <kato.hiroaki@nalgo.co.jp>
2021-02-05 11:07:09 +0900

    update: Comment

--- backend/tests/test_create_json.py
+++ backend/tests/test_create_json.py
@@ -1,20 +1,15 @@
-"""CreateJsonのテスト"""
+"""CreateJsonクラスのテスト"""
 
-import sys
 import unittest
 
 from src.create_json import CreateJson
 
-#path = os.path.join(os.path.dirname(__file__), '../src')
-#sys.path.append(path)
-
-#from create_json import CreateJson as cj  # noqa
-
 
 class TestCreateJson(unittest.TestCase):
-    # def setUp(self):
+    """CreateJsonクラスのテスト"""
 
     def test_get_last_row(self):
+        """get_last_rowメソッドのテスト"""
         json_creater = CreateJson([[], []], {})
 
         test_list = [[1, 2, 3], [3, 4, 5], [6, 7, 8]]
