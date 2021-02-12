2bd962b4c8e7b549aeea90393bfe8ea6da1deede
kato.hiroaki <kato.hiroaki@nalgo.co.jp>
2021-02-12 15:58:12 +0900

    Update: specified_room api

--- backend/src/fast_api.py
+++ backend/src/fast_api.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from fastapi import FastAPI
@@ -12,11 +13,10 @@ SETTING_FILE_NAME = 'setting.toml'
 
 app = FastAPI()
 log = Log()
+log.set_logger()
 
 
-@app.get('/')
-def main():
-    log.set_logger()
+def read_toml() -> dict:
     setting_file_name = os.path.join(os.path.dirname(__file__),
                                      SETTING_FILE_DIR, SETTING_FILE_NAME)
     log.logger.info(setting_file_name)
@@ -25,19 +25,42 @@ def main():
         toml_reader = FileReaderForToml()
         toml_reader.set_file_path(setting_file_name)
         toml_reader.load_file()
-        toml = toml_reader.get_contents()
+
+        return toml_reader.get_contents()
     except FileNotFoundError:
         return
+
+
+@app.get('/')
+def main() -> json:
+    toml = read_toml()
 
     json_creater = CreateJson(toml)
     json_creater.execute_create()
     created_json = jsonable_encoder(json_creater.get_created_json())
-
     log.logger.debug(created_json)
 
     return {'room_status': created_json}
 
 
-@app.post('/specified')
-def specified_room():
-    pass
+@app.get('/specified/{room}')
+def specified_room(room: str):
+    detect_field_num_key = 'detect_field_num'
+    toml = read_toml()
+
+    target_room = {}
+    target_room[detect_field_num_key] = toml[detect_field_num_key]
+
+    for area in toml['area']:
+        if area['場所'] == room:
+            # 処理の共通化のため、areaはリストで格納された辞書とする
+            target_room['area'] = [area]
+            break
+
+    json_creater = CreateJson(target_room)
+    json_creater.execute_create()
+    created_json = jsonable_encoder(json_creater.get_created_json())
+
+    log.logger.debug(created_json)
+
+    return {'specified_room_status': created_json}
