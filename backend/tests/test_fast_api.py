3d87e019e12fcf1147e0847b22cdf38235784977
kato.hiroaki <kato.hiroaki@nalgo.co.jp>
2021-02-12 17:19:30 +0900

    Update: '/' api test

--- backend/tests/test_fast_api.py
+++ backend/tests/test_fast_api.py
@@ -11,4 +11,32 @@ class TestFastAPI(unittest.TestCase):
     def test_main(self):
         response = self.client.get('/')
 
-        print(response)
+        expected = 200
+        actual = response.status_code
+        self.assertEqual(expected, actual)
+
+        expecteds = [{
+            '階数': '5階',
+            '利用者数': 2,
+            '上限超え': False,
+            '待ち人数': 10
+        }, {
+            '階数': '9階',
+            '利用者数': '',
+            '上限超え': '',
+            '待ち人数': ''
+        }, {
+            '階数': '11階',
+            '利用者数': '',
+            '上限超え': '',
+            '待ち人数': 8
+        }, {
+            '階数': '12階',
+            '利用者数': '',
+            '上限超え': '',
+            '待ち人数': 10
+        }]
+        actual = response.json()
+
+        for i in range(len(expecteds)):
+            self.assertEqual(expecteds[i], actual['room_status'][i])
