9d97415338da86dff9ec35aa5136694d8ef13403
kato.hiroaki <kato.hiroaki@nalgo.co.jp>
2021-02-16 15:17:12 +0900

    Update: implemented test_multiple_room

--- backend/tests/test_fast_api.py
+++ backend/tests/test_fast_api.py
@@ -94,5 +94,34 @@ class TestFastAPI(unittest.TestCase):
                 break
 
     def test_multiple_room(self):
-        response = self.client.get('/multiple?rooms=5階&rooms=9階')
-        pass
+        target_rooms = ['5階', '12階']
+        response = self.client.get(
+            f'/multiple?rooms={target_rooms[0]}&rooms={target_rooms[1]}')
+
+        expected = 200
+        actual = response.status_code
+        self.assertEqual(expected, actual)
+
+        expecteds = [{
+            '階数': '5階',
+            '利用者数': 2,
+            '上限超え': False,
+            '待ち人数': 10
+        }, {
+            '階数': '12階',
+            '利用者数': '',
+            '上限超え': '',
+            '待ち人数': 10
+        }]
+        response_json = response.json()
+
+        expected = dict
+        actual = type(response_json)
+
+        expected = len(expecteds[0])
+        actual = response_json['multiple_room_status']
+
+        self.assertEqual(expected, len(actual[0]))
+
+        for i in range(len(expecteds)):
+            self.assertEqual(expecteds[i], actual[i])
