31b1661bbf61b6fc7baf59ca8559d554d2db645e
kato.hiroaki <kato.hiroaki@nalgo.co.jp>
2021-02-16 11:56:18 +0900

    Update: implement test_specified_room

--- backend/tests/test_fast_api.py
+++ backend/tests/test_fast_api.py
@@ -50,4 +50,45 @@ class TestFastAPI(unittest.TestCase):
             self.assertEqual(expecteds[i], actual[i])
 
     def test_specified_room(self):
-        pass
+        target_room = '5階'
+        response = self.client.get(f'/specified/{target_room}')
+
+        expected = 200
+        actual = response.status_code
+        self.assertEqual(expected, actual)
+
+        expecteds = [{
+            '階数': '5階',
+            '利用者数': 2,
+            '上限超え': False,
+            '待ち人数': 10
+        }, {
+            '階数': '9階',
+            '利用者数': '',
+            '上限超え': '',
+            '待ち人数': ''
+        }, {
+            '階数': '11階',
+            '利用者数': '',
+            '上限超え': '',
+            '待ち人数': 8
+        }, {
+            '階数': '12階',
+            '利用者数': '',
+            '上限超え': '',
+            '待ち人数': 10
+        }]
+        response_json = response.json()
+
+        expected = dict
+        actual = type(response_json)
+
+        expected = len(expecteds[0])
+        actual = response_json['specified_room_status']
+
+        self.assertEqual(expected, len(actual[0]))
+
+        for i in range(len(expecteds)):
+            if expecteds[i]['階数'] == target_room:
+                self.assertEqual(expecteds[i], actual[i])
+                break
