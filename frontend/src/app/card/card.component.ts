a0560e55851a122afb68c840f7696bad1d9d0ffc
kato.hiroaki <kato.hiroaki@nalgo.co.jp>
2021-02-17 16:45:07 +0900

    Fix: card component design adjustment

--- frontend/src/app/card/card.component.ts
+++ frontend/src/app/card/card.component.ts
@@ -12,6 +12,7 @@ export class CardComponent implements OnInit {
   rooms: Room[] = [
     { room: '7階', use: '7', wait: '3', limit: '7' },
     { room: '9階', use: '3', wait: '0', limit: '10' },
+    { room: '12階', use: '10', wait: '10', limit: '10' },
   ];
 
   constructor() { }
