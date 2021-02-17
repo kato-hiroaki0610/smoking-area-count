91a3484883fce0d2bd65becee396431fa841e68c
kato.hiroaki <kato.hiroaki@nalgo.co.jp>
2021-02-17 15:36:12 +0900

    Update: load components for app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoomComponent } from './room/room.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
