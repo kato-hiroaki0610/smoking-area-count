4bcb3a9d35059ccae80c7bf279a2eb979519bd71
kato.hiroaki <kato.hiroaki@nalgo.co.jp>
2021-02-17 16:32:39 +0900

    Update: html to angular syntax

--- frontend/src/app/card/card.component.ts
+++ frontend/src/app/card/card.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from './mock-card';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
+
 export class CardComponent implements OnInit {
+
+  rooms: Room[] = [
+    { room: '7階', use: '7', wait: '3', limit: '7' },
+    { room: '9階', use: '3', wait: '0', limit: '10' },
+  ];
 
   constructor() { }
 
---
+++ frontend/src/app/card/mock-card.ts
@@ -0,0 +1,9 @@
+// mockデータ
+// TODO: room componentに移す
+//       正しいデータの形にする
+export interface Room {
+  room: string;
+  use: string;
+  wait: string;
+  limit: string;
+}
