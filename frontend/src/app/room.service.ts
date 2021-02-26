c27bafd405910e2be88eb1617ee379a99929eecd
kato.hiroaki <kato.hiroaki@nalgo.co.jp>
2021-02-26 12:11:37 +0900

    Update: implemented specified room

--- frontend/src/app/room.service.ts
+++ frontend/src/app/room.service.ts
@@ -14,25 +14,42 @@ export class RoomService {
   // それを回避するためフロントエンドのポート番号「4200」を指定し
   // Angular CLIのリバースプロキシを利用してバックエンドとの通信を実現する
   private host = 'http://localhost:4200/app';
-  private specifiedHost = 'http://localhost:4200/app/specified?room=';
-  private multipleHost = 'http://localhost:4200/app/multiple?room=';
+  private specifiedHost = 'http://localhost:4200/app/specified?';
+  private multipleHost = 'http://localhost:4200/app/multiple?';
 
   private state;
 
   constructor(private http: HttpClient) { }
 
-  getRooms(currentPath): Observable<Room[]> {
+  getRooms(currentPath: string): Observable<Room[]> {
+    console.log(currentPath);
+
+    // APIとqueryを分割する
+    const query = currentPath.split('?');
+
+    if (query[0] === '/') {
+      return this.getAllRooms();
+    } else if (query[0] === '/specified') {
+      const parameter = query[1];
+      return this.getSpecifiedRoom(parameter);
+    } else if (query[0] === '/multiple') {
+
+    } else {
+      return this.getAllRooms();
+    }
+
+    console.log(query);
     const rooms = this.http.get<Room[]>(this.host);
     return rooms;
   }
 
-  getAllRooms(currentPath): Observable<Room[]> {
+  getAllRooms(): Observable<Room[]> {
     const rooms = this.http.get<Room[]>(this.host);
     return rooms;
   }
 
-  getSpecifiedRoom(): Observable<Room[]> {
-    const room = this.http.get<Room[]>(this.specifiedHost);
+  getSpecifiedRoom(query: string): Observable<Room[]> {
+    const room = this.http.get<Room[]>(this.specifiedHost + query);
     return room;
   }
 
