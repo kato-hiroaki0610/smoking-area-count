4725c4f5c89ff7f0e7422222879701a0a1301b0d
kato.hiroaki <kato.hiroaki0610@gmail.com>
2021-03-18 14:05:39 +0900

    Fix: output move to pyinstaller option

--- convert_to_exe/convert_to_exe.bat
+++ convert_to_exe/convert_to_exe.bat
@@ -1,6 +1,5 @@
 @call activate pyinstaller
-@call pyinstaller fast_api.spec
-@call mv dist/fast_api.exe ../release
+@call pyinstaller --distpath ../release fast_api.spec
 @call rm -rf dist
 @call cp -r ../backend/src/setting ../release
 rem @call rm -rf build
