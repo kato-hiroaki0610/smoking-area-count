70a4e45f094e18f6b8b8d0c5dda604607506553d
kato.hiroaki <kato.hiroaki0610@gmail.com>
2021-04-21 17:23:38 +0900

    Update: room.serviceのテストを追加

--- frontend/src/app/room.service.spec.ts
+++ frontend/src/app/room.service.spec.ts
@@ -1,24 +1,93 @@
 import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Room, RoomStatus } from './card/room-info';
 
 import { RoomService } from './room.service';
 
 describe('RoomService', () => {
   let service: RoomService;
-  let httpClientSpy = jasmine.createSpyObj<HttpClient>(["get"]);
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
+        HttpClientTestingModule
       ],
       providers: [
-        { provide: HttpClient, useValue: httpClientSpy },
       ]
     });
     service = TestBed.inject(RoomService);
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('APIを含めたURLが正しく作成できること', () => {
+    const expected: string[] = [
+      'http://localhost:8000',
+      'http://localhost:8000/specified?room=5f',
+      'http://localhost:8000/multiple?room=5f&room=7f',
+      'http://localhost:8000',
+    ];
+    const api: string[] = ['', 'specified', 'multiple', 'hoge'];
+    const room: string[] = ['5f', '5f', '5f&room=7f', ''];
+
+    expected.forEach((value, i) => {
+      const actual = service.createAPI(api[i], room[i]);
+      expect(actual).toEqual(expected[i]);
+    });
+  });
+
+  it('APIが正しく呼べること', () => {
+    const apis: string[] = [
+      'http://localhost:8000',
+      'http://localhost:8000/specified?room=5f',
+      'http://localhost:8000/multiple?room=5f&room=9f',
+    ];
+
+    const expected: Room[][] = [
+      [
+        { room_status: [
+          { room: '5f', use: 5, limit: false, wait: 3 },
+          { room: '9f', use: 9, limit: false, wait: 2 },
+          { room: '12f', use: 2, limit: false, wait: 1 },
+        ]}
+      ],
+      [
+        { room_status: [
+          { room: '5f', use: 5, limit: false, wait: 3 },
+        ]}
+      ],
+      [
+        { room_status: [
+          { room: '5f', use: 5, limit: false, wait: 3 },
+          { room: '9f', use: 9, limit: false, wait: 2 },
+        ]}
+      ],
+    ];
+
+    apis.forEach((value, i) => {
+      service.callAPI(value).subscribe(actual => {
+        // `req.flush` で実際の検証を行う
+        expect(actual).toEqual(expected[i]);
+        expect(actual).toBeTruthy();
+      });
+
+      const req = httpTestingController.expectOne(value);
+      expect(req.request.method).toEqual('GET');
+
+      // サーバーからの疑似的な応答
+      // ここで指定した引数がactualに入る
+      req.flush(expected[i]);
+
+      // 未処理のリクエストがないかの確認
+      httpTestingController.verify();
+    });
   });
 });
