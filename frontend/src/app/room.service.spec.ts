19f7da0ef6e2f9c594a2b5661ccbb42b5a13746c
kato.hiroaki <kato.hiroaki0610@gmail.com>
2021-04-21 15:39:20 +0900

    Fix: room.serviceの自動生成のテストが成功するように修正

--- frontend/src/app/room.service.spec.ts
+++ frontend/src/app/room.service.spec.ts
@@ -1,16 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { RoomService } from './room.service';
 
 describe('RoomService', () => {
   let service: RoomService;
+  let httpClientSpy = jasmine.createSpyObj<HttpClient>(["get"]);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+      ],
+      providers: [
+        { provide: HttpClient, useValue: httpClientSpy },
+      ]
+    });
     service = TestBed.inject(RoomService);
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
   });
 });
