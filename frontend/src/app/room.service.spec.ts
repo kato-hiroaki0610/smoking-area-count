dabd32e887502b06bbc303f36f95ce4f2cb4726a
kato.hiroaki <kato.hiroaki0610@gmail.com>
2021-04-22 12:10:48 +0900

    Updatge: room.serviceのテストを追加

--- frontend/src/app/room.service.spec.ts
+++ frontend/src/app/room.service.spec.ts
@@ -40,11 +40,11 @@ describe('RoomService', () => {
 
     expected.forEach((value, i) => {
       const actual = service.createAPI(api[i], room[i]);
-      expect(actual).toEqual(expected[i]);
+      expect(actual).toEqual(value);
     });
   });
 
-  it('APIが正しく呼べること', () => {
+  it('callAPIメソッドからAPIが正しく呼べること', () => {
     const apis: string[] = [
       'http://localhost:8000',
       'http://localhost:8000/specified?room=5f',
@@ -85,6 +85,56 @@ describe('RoomService', () => {
       // サーバーからの疑似的な応答
       // ここで指定した引数がactualに入る
       req.flush(expected[i]);
+
+      // 未処理のリクエストがないかの確認
+      httpTestingController.verify();
+    });
+  });
+
+  it('getRoomsメソッドからAPIが正しく呼べていること', () => {
+    const api: string[] = ['', 'specified', 'multiple', 'hoge'];
+    const room: string[] = ['5f', '5f', '5f&room=9f', ''];
+    const expected: Room[][] = [
+      [
+        { room_status: [
+          { room: '5f', use: 5, limit: false, wait: 3 },
+          { room: '9f', use: 9, limit: false, wait: 2 },
+          { room: '12f', use: 2, limit: false, wait: 1 },
+        ]}
+      ],
+      [
+        { room_status: [
+          { room: '5f', use: 5, limit: false, wait: 3 },
+        ]}
+      ],
+      [
+        { room_status: [
+          { room: '5f', use: 5, limit: false, wait: 3 },
+          { room: '9f', use: 9, limit: false, wait: 2 },
+        ]}
+      ],
+    ];
+    const url: string[] = [
+      'http://localhost:8000',
+      'http://localhost:8000/specified?room=5f',
+      'http://localhost:8000/multiple?room=5f&room=9f',
+      'http://localhost:8000'
+    ];
+
+
+    expected.forEach((value, i) => {
+      service.getRooms(api[i], room[i]).subscribe(actual => {
+        // `req.flush` で実際の検証を行う
+        expect(actual).toEqual(expected[i]);
+        expect(actual).toBeTruthy();
+      });
+
+      const req = httpTestingController.expectOne(url[i]);
+      expect(req.request.method).toEqual('GET');
+
+      // サーバーからの疑似的な応答
+      // ここで指定した引数がactualに入る
+      req.flush(value);
 
       // 未処理のリクエストがないかの確認
       httpTestingController.verify();
