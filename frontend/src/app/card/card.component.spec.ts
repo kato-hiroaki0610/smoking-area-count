d9cc77f0f15fde45da55316a0b9b6d094e7f4325
kato.hiroaki <kato.hiroaki0610@gmail.com>
2021-04-28 17:26:48 +0900

    Update: Card.componentのテストを追加

--- frontend/src/app/card/card.component.spec.ts
+++ frontend/src/app/card/card.component.spec.ts
@@ -1,8 +1,11 @@
+import { RoomService } from './../room.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
 import { ActionSequence } from 'selenium-webdriver';
 
 import { CardComponent } from './card.component';
+import { Room } from './room-info';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -25,7 +28,7 @@ describe('CardComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('getAPI', () => {
+  it('getAPIメソッドで期待するAPIが取得できること', () => {
     const api = [
       {'/': true, specified: false, multiple: false},
       {'/': false, specified: true, multiple: false},
@@ -42,16 +45,17 @@ describe('CardComponent', () => {
     });
   });
 
-  it('QueryParameterで指定された部屋ができること', () => {
-    const api = ['', 'specified', 'specified', 'multiple'];
+  it('getRoomメソッドを呼びQueryParameterで指定した部屋が取得できること', () => {
+    const api = ['', '', 'specified', 'specified', 'multiple'];
     const rooms = [
+                    [''],
                     ['5f'],
                     ['7f'],
                     ['9f', '12f'],
                     ['5f', '9f', '12f'],
     ];
 
-    const expected = ['5f', '7f', '9f', '5f&room=9f&room=12f'];
+    const expected = ['', '', '?room=7f', '?room=9f', '?room=5f&room=9f&room=12f'];
 
     expected.forEach((value, i) => {
       const actual = component.getRoom(api[i], rooms[i]);
@@ -59,7 +63,33 @@ describe('CardComponent', () => {
     });
   });
 
-  xit('getRooms', () => {
-    fixture.detectChanges();
+  it('getRooms', async (done: DoneFn) => {
+    let httpClient: HttpClient;
+    httpClient = TestBed.inject(HttpClient);
+    const testGetRoom = 'Test GetRoom';
+    // const roomService = jasmine.createSpyObj('RoomService', ['getRooms']);
+    const roomService = new RoomService(httpClient);
+
+    const expected: Room[] = [
+        { room_status: [
+          { room: '5f', use: 5, limit: false, wait: 3 },
+          { room: '9f', use: 9, limit: false, wait: 2 },
+          { room: '12f', use: 2, limit: false, wait: 1 },
+        ]}
+    ];
+
+    spyOn(roomService, 'getRooms')
+    .and.returnValue(of(expected));
+
+    const cardComponent = new CardComponent(roomService);
+    await cardComponent.getRooms();
+
+    // const result = component.roomStatus;
+    const result = cardComponent.roomStatus;
+    done();
+    console.log(result);
+    roomService.getRooms('hoge', 'fuga').subscribe((r) => {
+      console.log(r);
+    });
   });
 });
