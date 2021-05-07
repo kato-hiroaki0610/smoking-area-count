95015466b0e59174073fd5fa26d2c080abd458ff
kato.hiroaki <kato.hiroaki0610@gmail.com>
2021-05-07 16:34:26 +0900

    Update: CSVの最終行が更新された際に変更されているか確認を行うテストを追加

--- e2e_test/test_e2e.py
+++ e2e_test/test_e2e.py
@@ -17,7 +17,7 @@ class TestSmokingAreaCount(object):
 
     def open_csv(self, file_path):
         """CSVを開く"""
-        with open(file_path, 'r', encoding='utf8') as f:
+        with open(file_path, 'r', newline='', encoding='utf8') as f:
             reader = csv.reader(f)
             # iteratorをリストに変換する
             rows = [r for r in reader]
@@ -43,9 +43,14 @@ class TestSmokingAreaCount(object):
 
         toml_data = toml.load(open(f'{setting_dir}\\{setting_file}',
                               encoding='utf8'))
-        csv_path = str(pathlib.Path.cwd() / csv_file_name)
-        toml_data['area'][index][is_use_or_wait] = csv_path
-        with open(f'{setting_dir}\\{setting_file}', 'w',
+        # 'empty'の場合は空文字列「""」を代入する
+        if csv_file_name == 'empty':
+            toml_data['area'][index][is_use_or_wait] = ''
+        else:
+            csv_path = str(pathlib.Path.cwd() / csv_file_name)
+            toml_data['area'][index][is_use_or_wait] = csv_path
+        print(toml_data)
+        with open(f'{setting_dir}\\{setting_file}', 'w', newline='',
                   encoding='utf8') as fp:
             toml.dump(toml_data, fp)
 
@@ -107,7 +112,6 @@ class TestSmokingAreaCount(object):
         tags = ['room_use_wrap', 'room_wait_wrap']
         for i in range(len(tags)):
             elements = self.driver.find_elements_by_class_name(tags[i])
-
             expecteds = [['5人', '20人', '100人'], ['5人', '20人', '0人']]
             for j in range(len(expecteds[0])):
                 element = elements[j]
@@ -134,7 +138,11 @@ class TestSmokingAreaCount(object):
         wait = '待ち人数'
         rewrite_data = [
                 {'path': csv_files[0], 'index': 0, 'use_or_wait': use},
-                {'path': csv_files[1], 'index': 0, 'use_or_wait': wait}
+                {'path': csv_files[1], 'index': 0, 'use_or_wait': wait},
+                {'path': csv_files[0], 'index': 1, 'use_or_wait': use},
+                {'path': csv_files[1], 'index': 1, 'use_or_wait': wait},
+                {'path': csv_files[0], 'index': 2, 'use_or_wait': use},
+                {'path': csv_files[1], 'index': 2, 'use_or_wait': wait}
             ]
         for i in rewrite_data:
             self.rewrite_csv_path(i['path'], i['index'], i['use_or_wait'])
@@ -167,11 +175,103 @@ class TestSmokingAreaCount(object):
 
         for f in csv_files:
             self.remove_csv(f)
-        pass
 
     def test_renewal_csv_lastline(self):
-        """CSVの更新されたらリアルタイムに反映されること"""
-        pass
+        """CSVの最終行が更新されたらリアルタイムに反映されること"""
+        csv_data = {
+                    'tmp.csv': [['a', 'b', 'c', 'd', '5'],
+                                ['a', 'b', 'c', 'd', '5']],
+                    'tmp2.csv': [['a', 'b', 'c', 'd', '7']]
+        }
+        csv_files = ['tmp.csv', 'tmp2.csv']
+        for f in csv_files:
+            self.create_csv(csv_data[f], f)
+
+        use = '利用者'
+        wait = '待ち人数'
+        rewrite_data = [
+                {'path': csv_files[0], 'index': 0, 'use_or_wait': use},
+                {'path': csv_files[1], 'index': 0, 'use_or_wait': wait}
+            ]
+        for i in rewrite_data:
+            self.rewrite_csv_path(i['path'], i['index'], i['use_or_wait'])
+        time.sleep(5)
+
+        tags = ['room_use_wrap', 'room_wait_wrap']
+        expecteds = [['5人', '7人'], ['10人', '3人']]
+        for i in range(len(csv_files)):
+            elements_for_use = \
+                self.driver.find_elements_by_class_name(tags[0])
+            elements_for_wait = \
+                self.driver.find_elements_by_class_name(tags[1])
+
+            detects_for_use = \
+                elements_for_use[0].find_elements_by_tag_name('div')
+            detect_for_use = \
+                detects_for_use[1].get_attribute('innerHTML')
+            detects_for_wait = \
+                elements_for_wait[0].find_elements_by_tag_name('div')
+            detect_for_wait = \
+                detects_for_wait[1].get_attribute('innerHTML')
+
+            assert expecteds[i][0] == detect_for_use
+            assert expecteds[i][1] == detect_for_wait
+
+            # 最終行を追加
+            add_csv_data = {
+                'tmp.csv': ['a', 'b', 'c', 'd', '10'],
+                'tmp2.csv': ['a', 'b', 'c', 'd', '3']
+            }
+            for csv_file in csv_files:
+                with open(csv_file, 'a', encoding='utf8', newline='') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(add_csv_data[csv_file])
+
+            time.sleep(5)
+
+        for f in csv_files:
+            self.remove_csv(f)
+
+    def test_not_exists_csv_file(self):
+        """設定ファイルに誤ったCSVパスが格納されている場合0人と表示されること"""
+        csv_data = {
+                    'tmp.csv': [['a', 'b', 'c', 'd', '5'],
+                                ['a', 'b', 'c', 'd', '6'],
+                                ['a', 'b', 'c', 'd', '2']]
+        }
+        csv_files = ['tmp.csv']
+        for f in csv_files:
+            self.create_csv(csv_data[f], f)
+
+        use = '利用者'
+        wait = '待ち人数'
+        # emptyと書かれているものは空文字列「""」を表す
+        rewrite_data = [
+                {'path': csv_files[0], 'index': 0, 'use_or_wait': use},
+                {'path': csv_files[0], 'index': 0, 'use_or_wait': wait},
+                {'path': 'hoge.csv', 'index': 1, 'use_or_wait': use},
+                {'path': 'fuga.csv', 'index': 1, 'use_or_wait': wait},
+                {'path': 'empty', 'index': 2, 'use_or_wait': use},
+                {'path': 'empty', 'index': 2, 'use_or_wait': wait},
+            ]
+        for i in rewrite_data:
+            self.rewrite_csv_path(i['path'], i['index'], i['use_or_wait'])
+        time.sleep(10)
+
+        tags = ['room_use_wrap', 'room_wait_wrap']
+        expecteds = ['2人', '0人', '0人']
+        for i in range(len(tags)):
+            elements = self.driver.find_elements_by_class_name(tags[i])
+
+            for j in range(len(expecteds)):
+                print(j)
+                element = elements[j]
+                detects = element.find_elements_by_tag_name('div')
+                detect = detects[1].get_attribute('innerHTML')
+                assert expecteds[j] == detect
+
+        for f in csv_files:
+            self.remove_csv(f)
 
     def teardown_method(self):
         # ブラウザを終了する
